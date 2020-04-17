f0655a2f43120b723d7bb3a23fd604c38c981da9
jhemesonmotta <jhemesonmotta@gmail.com>
2020-04-17 19:56:32 -0300

    analisando com dados 2019

--- apriori/gsoc_female.py
+++ apriori/gsoc_female.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 21 15:39:36 2019
-
-@author: IMGADMIN
-"""
-
 import pandas as pd
+from apyori import apriori
+
 dadosFemale = pd.read_csv('gsoc_female.csv', header = None)
 
 transacoes = []
-for i in range (0,2437):  
+for i in range (0,3212):  
     transacoes.append([str(dadosFemale.values[i, j]) for j in range (0,3)])
     
-from apyori import apriori
 regras = apriori(transacoes, min_support = 0.005, min_confidence = 0.7, min_lift = 3, min_length = 2)
 
 resultados = list(regras)
@@ -20,13 +15,14 @@ resultados = list(regras)
 resultados2 = [list(x) for x in resultados]
 resultadoFormatado = []
 
-for j in range(0,40):
+for j in range(0,31):
     resultadoFormatado.append([list(x) for x in resultados2[j][2]])
     
-for z in range (0,40):
+for z in range (0,31):
     print("\n")
     print("\n")
     print(resultadoFormatado[z])
     print("\n")
     print("\n")
+    
 resultadoFormatado
--- apriori/gsoc_male.py
+++ apriori/gsoc_male.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 21 15:39:36 2019
-
-@author: IMGADMIN
-"""
-
 import pandas as pd
+from apyori import apriori
+
 dadosMale = pd.read_csv('gsoc_male.csv', header = None)
 
 transacoes = []
-for i in range (0,5980):  
+for i in range (0,8523):  
     transacoes.append([str(dadosMale.values[i, j]) for j in range (0,3)])
-    
-from apyori import apriori
+
 regras = apriori(transacoes, min_support = 0.005, min_confidence = 0.7, min_lift = 3, min_length = 2)
 
 resultados = list(regras)
@@ -20,11 +15,10 @@ resultados = list(regras)
 resultados2 = [list(x) for x in resultados]
 resultadoFormatado = []
 
-for j in range(0,44):
+for j in range(0,36):
     resultadoFormatado.append([list(x) for x in resultados2[j][2]])
-
   
-for z in range (0,44):
+for z in range (0,36):
     print("\n")
     print("\n")
     print(resultadoFormatado[z])
